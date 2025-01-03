490ab5d448dc807d2434d37c1398e298dae29525
md.munna <mk8761174@gmail.com>
2025-01-03 14:33:00 +0600

    final project part 4 practice done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  app.use(cors())
  app.use(express.json())
 
- const { MongoClient, ServerApiVersion } = require('mongodb');
+ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gamza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
  
  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,13 +38,21 @@ app.get('/reviews', async(req,res)=>{
 })
 // carts collections
 app.get('/carts', async(req,res)=>{
-  const result= await cartsCollection.find().toArray()
+  const email=req.query.email
+  const query={email:email}
+  const result= await cartsCollection.find(query).toArray()
   res.send(result)
 })
 app.post('/carts',async(req,res)=>{
 const cart=req.body
 const result=await cartsCollection.insertOne(cart)
 res.send(result)
+})
+app.delete('/carts/:id',async(req,res)=>{
+  const id = req.params.id
+  const query= {_id: new ObjectId(id)}
+  const result= cartsCollection.deleteOne(query)
+  res.send(result)
 })
 
 
